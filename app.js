2bf3946204c3e9e36d351c346a1ca05cb159f673
John Rapp <johnrapp814@gmail.com>
2018-09-07 14:02:47 -0400

    adding color switching nav text, increased gradient on hero to increase contrast on text

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var navbar = document.querySelector('nav');
+var navBox = navbar.getBoundingClientRect();
+var lightSection = document.querySelector('.light-bg');
+var lightSectionBox = lightSection.getBoundingClientRect();
 var lastScroll = 0;
 
 window.onscroll = () => {
+  toggleNav();
+  swapNavColor();
+};
+
+toggleNav = () => {
   var currentY = window.pageYOffset;
   if (lastScroll > currentY || lastScroll === 0) {
     navbar.style.top = '0';
@@ -9,4 +17,23 @@ window.onscroll = () => {
     navbar.style.top = '-100px';
   }
   lastScroll = currentY;
+};
+
+swapNavColor = () => {
+  var lightTop = lightSectionBox.top;
+  var lightBottom = lightSectionBox.bottom;
+  var currentY = window.pageYOffset + 15;
+
+  if (
+    currentY > lightTop &&
+    currentY < lightBottom &&
+    !navbar.classList.contains('dark-text')
+  ) {
+    navbar.classList.add('dark-text');
+  } else if (
+    (currentY > lightBottom || currentY < lightTop) &&
+    navbar.classList.contains('dark-text')
+  ) {
+    navbar.classList.remove('dark-text');
+  }
 };
